5dc1c02d107f8e7816a86f15b46148bfc256dd23
Facfac5000-git <facfac4000@hotmail.com>
2021-06-14 18:28:42 -0300

    Sumatoria de productos disponibles

--- inventory/views.py
+++ inventory/views.py
@@ -13,11 +13,19 @@ def dashboard(request):
     total_supplier = Supplier.objects.filter(active=True).count()
     total_order = Order.objects.filter(created_date=datetime.now().date()).count()
     orders = Order.objects.all().order_by('-id')
+    products = Product.objects.filter(active=True)
+    active_products_list_price = 0
+    active_products_selling_price = 0
+    for product in products:
+        active_products_list_price = active_products_list_price + (product.stock * product.list_price)
+        active_products_selling_price = active_products_selling_price + (product.stock * product.price)
     context = {
         'product': total_product,
         'supplier': total_supplier,
         'buyer': total_buyer,
         'order': total_order,
         'orders': orders,
+        'active_products_list_price': active_products_list_price,
+        'active_products_selling_price': active_products_selling_price,
     }
     return render(request, 'dashboard.html', context)
